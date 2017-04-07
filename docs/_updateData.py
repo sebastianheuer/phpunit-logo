f5343ac9510ccae17cd5d125538e3a803e79c21f
Sebastian Heuer <sebastian@phpeople.de>
2017-04-07 13:55:59 +0200

    do not regenerate data yaml from scratch every time to allow addition of comments by hand

--- docs/_updateData.py
+++ docs/_updateData.py
@@ -3,8 +3,15 @@ import os
 import re
 
 submissionsDir = '../suggestions'
+dataFilename = '_data/submissions.yml'
 
 data = []
+
+with open(dataFilename, 'r') as stream:
+    try:
+        data = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
 
 for subdir, dirs, files in os.walk(submissionsDir):
     for file in files:
@@ -12,9 +19,18 @@ for subdir, dirs, files in os.walk(submissionsDir):
             continue
         fullPath = os.path.join(subdir, file).replace('../', '/')
         authorGitHubAlias = re.findall(r'/suggestions/(.*)/', fullPath)[0]
+
+        found = False
+
+        for index, item in enumerate(data):
+            if item['path'] == fullPath:
+                found = True
+
+        if found:
+            continue
+
+        print "append"
         data.append({'path': fullPath, 'author': authorGitHubAlias});
-
-
 
 with open('_data/submissions.yml', 'w') as outfile:
     yaml.dump(data, outfile, default_flow_style=False)
